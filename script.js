b2b2c8d632de4ec3b271d7088bcc9b0cacf3e7df
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-17 19:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,123 @@
-//your JS code here. If required.
-let zero = document.querySelector(".number-0");
-/* Numbers */ 
-let numbers = document.querySelectorAll(".numbers");
-/* Operators */
-let operators = document.querySelectorAll(".operators");
-/* equals */
-let equals = document.querySelector(".equals");
-/* clear */ 
-let clear = document.querySelector(".clear");
-/* decimal */ 
-let decimal = document.querySelector(".decimal");
-/* input */
-let input = document.querySelector(".input");
+let output = document.getElementById("output")
+let logic = document.getElementById("logic")
 
-var clearSwitch = 0;
-zero.addEventListener("click", displayZero);
-function displayZero() {
-  input.innerText += zero.innerText;
+
+function numberOne() {
+    let logicOperator = 1
+    logic.value += logicOperator
 }
-
-clear.addEventListener("click", clearInput);
-function clearInput() {
-  input.innerText = "";
+function numberTwo() {
+    let logicOperator = 2
+    logic.value += logicOperator
 }
-
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", () => {
-
-    input.innerText += numbers[i].innerText;
-    console.log(typeof numbers[i].innerText)
-  });
+function numberThree() {
+    let logicOperator = 3
+    logic.value += logicOperator
 }
-
-decimal.addEventListener('click', ()=>{
-  if (input.innerText != "Enter the numbers") {
-    input.innerText += decimal.innerText;
-  }
-})
-
-for (let i = 0; i < operators.length; i++) {
-  operators[i].addEventListener('click', ()=>{
-    if (input.innerText == "Enter the numbers") {
-      input.innerText = "";
+function numberFour() {
+    let logicOperator = 4
+    logic.value += logicOperator
+}
+function numberFive() {
+    let logicOperator = 5
+    logic.value += logicOperator
+}
+function numberSix() {
+    let logicOperator = 6
+    logic.value += logicOperator
+}
+function numberSeven() {
+    let logicOperator = 7
+    logic.value += logicOperator
+}
+function Eight() {
+    let logicOperator = 8
+    logic.value += logicOperator
+}
+function numberNine() {
+    let logicOperator = 9
+    logic.value += logicOperator
+}
+function numberZero() {
+    let logicOperator = 0
+    logic.value += logicOperator
+}
+function numberCurll() {
+    let logicOperator = "*("
+    logic.value += logicOperator
+}
+function numberCurlr() {
+    let logicOperator = ")"
+    logic.value += logicOperator
+}
+// Operators and logic below
+function plus() {
+    if (logic.value.length < 1) {
+        let logicOperator = "0+"
+        logic.value += logicOperator
+    } 
+    if (!logic.value.endsWith("+")&&!logic.value.endsWith("*")&&!logic.value.endsWith("/")) {
+        let logicOperator = "+"
+        logic.value += logicOperator
     }
-    input.innerText += operators[i].innerText;
-    });
+        
 }
-equals.addEventListener('click', () => {
-  input.innerText = eval(input.innerText);
-})
 
+function minus() {
+    if (logic.value.length < 1) {
+        let logicOperator = "0-"
+        logic.value += logicOperator
+    } 
+    if (!logic.value.endsWith("-")&&!logic.value.endsWith("*")&&!logic.value.endsWith("/")) {
+        let logicOperator = "-"
+        logic.value += logicOperator
+    }
+        
+}
+
+function multiply() {
+    if (logic.value.length < 1) {
+        let logicOperator = "0*"
+        logic.value += logicOperator
+    } 
+    if (!logic.value.endsWith("*")&&!logic.value.endsWith("+")&&!logic.value.endsWith("/")&&!logic.value.endsWith("-")&&!logic.value.endsWith("(")) {
+        let logicOperator = "*"
+        logic.value += logicOperator
+    }
+        
+}
+
+function divide() {
+    if (logic.value.length < 1) {
+        let logicOperator = "0/"
+        logic.value += logicOperator
+    } 
+    if (!logic.value.endsWith("/")&&!logic.value.endsWith("+")&&!logic.value.endsWith("*")&&!logic.value.endsWith("-")&&!logic.value.endsWith("(")) {
+        let logicOperator = "/"
+        logic.value += logicOperator
+    }
+        
+}
+function ac() {
+    logic.value = ""
+    output.textContent = 0  
+}
+function displayResult() {   
+    if (logic.value === "") {
+        let logicOperator = "0"
+        logic.value += logicOperator
+        output.textContent = eval(logic.value)
+        logic.value = ""
+     } 
+     else{
+        output.textContent = eval(logic.value)
+        logic.value = ""   
+     }
+
+}
+
+function deleteCha() {
+    let str = logic.value
+    str = str.slice(0, -1); 
+    logic.value = str
+}
